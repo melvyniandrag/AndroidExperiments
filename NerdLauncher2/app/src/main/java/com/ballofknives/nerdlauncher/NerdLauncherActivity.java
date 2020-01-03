b55da1d29fb9e9201e945f27a104fda01eb4ebb5
Melvyniandrag <melvyniandrag@gmail.com>
2020-01-03 17:51:50 -0500

    added new project for chapter 23

---
+++ NerdLauncher2/app/src/main/java/com/ballofknives/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.ballofknives.nerdlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
---
+++ NerdLauncher2/app/src/main/java/com/ballofknives/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,6 @@
+package com.ballofknives.nerdlauncher;
+
+import androidx.fragment.app.ListFragment;
+
+public class NerdLauncherFragment extends ListFragment {
+}
